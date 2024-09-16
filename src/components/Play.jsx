c7bd29f712f6263706f1ec28935539161fcd91b8
Purvi Porwal <purviporwal1812@gmail.com>
2024-09-16 12:39:18 +0530

    image

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,7 +1,15 @@
-const Play = () =>{
-    return(<>
-    slayyyy cominggg soon.
-    </>
-    )
+import React from 'react';
+import i1 from '../assets/i1.jpeg'; 
+
+const Play = () => {
+    return (
+        <>
+            <div>
+                <img src={i1} alt="NCC Certificate" style={{ width: '300px', height: 'auto' }} />
+                <h6>secured an A-certificate in NCC</h6>
+            </div>
+        </>
+    );
 }
+
 export default Play;
